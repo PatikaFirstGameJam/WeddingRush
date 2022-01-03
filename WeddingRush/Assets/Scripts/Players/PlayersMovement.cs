1b13ed17892021f918cd2f6c1539042e2de0c959
Olcay ÇİFTÇİ <olcay_farmer@hotmail.com>
2022-01-03 19:38:57 +0300

    Girl and Boy Character Movements

---
+++ WeddingRush/Assets/Scripts/Players/PlayersMovement.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject girlObject;
+    [SerializeField] private GameObject boyObject;
+
+    [SerializeField] private Transform boySideMovementRoot;
+    [SerializeField] private Transform girlSideMovementRoot;
+
+    [SerializeField] private Transform boyLeftLimit;
+    [SerializeField] private Transform boyRightLimit;
+
+    [SerializeField] private Transform girlLeftLimit;
+    [SerializeField] private Transform girlRightLimit;
+
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float sideMovementSensitivity = 0.01f;
+
+
+    private Vector2 inputDrag;
+
+    private Vector2 previousMousePosition;
+
+    private float boyLeftLimitX => boyLeftLimit.localPosition.x;
+    private float boyRightLimitX => boyRightLimit.localPosition.x;
+
+    private float girlLeftLimitX => girlLeftLimit.localPosition.x;
+    private float girlRightLimitX => girlRightLimit.localPosition.x;
+    void Update()
+    {
+        ForwardMovement();
+        HandleInput();
+        SideMovement();
+    }
+
+    private void ForwardMovement()
+    {
+        BoyForwardMovement();
+        GirlForwardMovement();
+    }
+    
+    private void BoyForwardMovement()
+    {
+        boyObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void GirlForwardMovement()
+    {
+        girlObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void SideMovement()
+    {
+        BoySideMovement();
+        GirlSideMovement();
+    }
+
+    private void BoySideMovement()
+    {
+        var localPosBoy = boySideMovementRoot.localPosition;
+        localPosBoy += Vector3.right * inputDrag.x * sideMovementSensitivity;
+        localPosBoy.x = Mathf.Clamp(localPosBoy.x, boyLeftLimitX, boyRightLimitX);
+        boySideMovementRoot.localPosition = localPosBoy;
+    }
+
+    private void GirlSideMovement()
+    {
+        var localPosGirl = girlSideMovementRoot.localPosition;
+        localPosGirl -= Vector3.right * inputDrag.x * sideMovementSensitivity;
+        localPosGirl.x = Mathf.Clamp(localPosGirl.x, girlLeftLimitX, girlRightLimitX);
+        girlSideMovementRoot.localPosition = localPosGirl;
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            previousMousePosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            var deltaMouse = (Vector2)Input.mousePosition - previousMousePosition;
+            inputDrag = deltaMouse;
+            previousMousePosition = Input.mousePosition;
+        }
+        else
+        {
+            inputDrag = Vector2.zero;
+        }
+    }
+}

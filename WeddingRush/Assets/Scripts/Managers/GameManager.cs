dd3148570f7f67dd3b6eec39be9a6c0381f87e50
Olcay ÇİFTÇİ <olcay_farmer@hotmail.com>
2022-01-04 15:41:39 +0300

    increase and decrease love added

--- WeddingRush/Assets/Scripts/Managers/GameManager.cs
+++ WeddingRush/Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class GameManager : MonoSingleton<GameManager>
 {
     [SerializeField] private int money = 0;
+    [SerializeField] private int love = 50;
     [SerializeField] private GameObject girlPlayer;
     [SerializeField] private bool isGirlEnable = true;
 
@@ -12,15 +13,26 @@ public class GameManager : MonoSingleton<GameManager>
         money += 200;
     }
 
+    public void IncreaseLove()
+    {
+        love += 10;
+    }
+
+    public void DecreaseLove()
+    {
+        love -= 20;
+    }
+
     public void DecreaseRingAmount()
     {
         if (money >= 500)
         {
+            IncreaseLove();
             money -= 500;
         }
         else
         {
-            Debug.Log("Love Decrease -50");
+            DecreaseLove();
         }
     }
 
